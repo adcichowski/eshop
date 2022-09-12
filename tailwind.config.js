498b7bb6b7a07c9560d901ad529db56517e8ccb3
Adam Cichowski <adam.cichowski98@outlook.com>
2022-09-12 20:48:25 +0200

    fix: problem with media queries in new files src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     colors: {
       primary: "#76A765",
