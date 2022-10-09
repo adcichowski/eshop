bf099cb82026cc19c0a828954fe375e1168bee6a
Adam Cichowski <adam.cichowski98@outlook.com>
2022-10-09 13:07:25 +0200

    fix: repair product offer view

--- components/ProductOffer/ProductOffer.tsx
+++ components/ProductOffer/ProductOffer.tsx
@@ -13,33 +13,29 @@ export function ProductOffer({
 }) {
   return (
     <div>
-      <section
+      <article
         aria-describedby={`poster${name}`}
         className="flex flex-col items-center"
       >
-        <div>
-          <Image
-            className="border-2 border-black"
-            {...image}
-            width={218.63}
-            height={323.46}
-          />
-          <div className="ml-1">
-            <div className="flex flex-col">
-              <HeaderTag id={`poster${name}`}>{name}</HeaderTag>
-              <div className="font-medium mb-1">od 10zł</div>
-              <div className="grid grid-cols-2 mb-2">
-                <button className="text-xs rounded-none bg-primary px-4 py-2 text-white uppercase self-center">
-                  Add to cart
-                </button>
-                <div className="justify-self-end mr-5 relative">
-                  <FavoriteInput id={name} />
-                </div>
-              </div>
+        <Image
+          className="border-2 border-black"
+          {...image}
+          width={218.63}
+          height={323.46}
+        />
+        <div className="pl-6">
+          <HeaderTag id={`poster${name}`}>{name}</HeaderTag>
+          <div className="font-medium mb-1">od 10zł</div>
+          <div className="grid grid-cols-2 mb-2">
+            <button className="text-xs rounded-none bg-primary px-4 py-2 text-white uppercase self-center">
+              Add to cart
+            </button>
+            <div className="justify-self-end mr-8 relative">
+              <FavoriteInput id={name} />
             </div>
           </div>
         </div>
-      </section>
+      </article>
     </div>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,42 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <link
-        rel="preload"
-        href="/fonts/Montserrat-Bold.ttf"
-        as="font"
-        type="font/ttf"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="/fonts/Montserrat-Light.ttf"
-        as="font"
-        type="font/ttf"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="/fonts/Montserrat-Medium.ttf"
-        as="font"
-        type="font/ttf"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="/fonts/Montserrat-Regular.ttf"
-        as="font"
-        type="font/ttf"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        href="/fonts/Montserrat-Semibold.ttf"
-        as="font"
-        type="font/ttf"
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <body className="bg-gray-100">
         <Main />
         <NextScript />
