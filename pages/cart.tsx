6290571958a22ac34e91c98789bbfefa5f22061c
Adam Cichowski <adam.cichowski98@outlook.com>
2023-01-01 20:38:56 +0100

    fix: repair build app

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,20 +1,8 @@
-import { useCartContext } from "context/CartContext";
-import React from "react";
-
 export default function CartPage() {
-  const { items } = useCartContext();
   return (
     <main className="flex flex-grow">
       <div className="flex flex-grow items-center justify-center">
-        {items.length ? (
-          items.map((item) => (
-            <h2 className="text-2xl" key={item.title}>
-              item.
-            </h2>
-          ))
-        ) : (
-          <h1 className="text-2xl">You dont have any poster in cart!</h1>
-        )}
+        <h1 className="text-2xl">You dont have any poster in cart!</h1>
       </div>
     </main>
   );
