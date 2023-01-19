72138a7c1319e4fa159fa0330056fa21148a25ba
Adam Cichowski <adam.cichowski98@outlook.com>
2023-01-19 23:56:54 +0100

    fix: add type to json parse

--- context/CartContext/CartContext.tsx
+++ context/CartContext/CartContext.tsx
@@ -1,7 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import type { CartContextType, CartItem } from "./types";
 import { addProductToCart, deleteProductFromCart } from "./utilsCart";
-
 const CartContext = createContext<CartContextType | null>(null);
 
 export const CartContextProvider = ({
@@ -12,7 +11,13 @@ export const CartContextProvider = ({
   const [cart, setCart] = useState<Record<string, CartItem> | undefined>(
     undefined
   );
+  useEffect(() => {
+    setCart(getCartFromLocalStorage());
+  }, []);
 
+  useEffect(() => {
+    setCartInLocalStorage(cart);
+  }, [cart]);
   return (
     <CartContext.Provider
       value={{
@@ -36,4 +41,22 @@ export const useCartContext = () => {
     throw new Error("Wrap components using CartContextProvider");
   }
   return cartContext;
+};
+
+const getCartFromLocalStorage = () => {
+  try {
+    const cartLocalStorage = localStorage.getItem("cart");
+    if (cartLocalStorage === null) {
+      return undefined;
+    }
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- i will provide schema to validate this
+    return JSON.parse(cartLocalStorage) as Record<string, CartItem>;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+const setCartInLocalStorage = (cart: Record<string, CartItem> | undefined) => {
+  if (cart === undefined) return;
+  return localStorage.setItem("cart", JSON.stringify(cart));
 };
