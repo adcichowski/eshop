a8273fa8d319893ef230e1d5c546a2a0c9a6ed9b
Adam Cichowski <adam.cichowski98@outlook.com>
2023-01-22 16:13:02 +0100

    feat: add clicable product offer

--- components/ProductsCarrousel/components/ProductCarrousel/ProductCarrousel.tsx
+++ components/ProductsCarrousel/components/ProductCarrousel/ProductCarrousel.tsx
@@ -23,37 +23,43 @@ export const ProductCarrousel = ({
   images,
 }: ProductCarrouselProps) => {
   return (
-    <Link passHref href={slug}>
-      <section aria-labelledby={name} className="flex flex-col">
-        <div>
-          <div className="max-w-[206px] m-auto max-h-72 object-center overflow-hidden">
-            <Image
-              alt={name}
-              src={images[0].url}
-              width={180}
-              height={251}
-              className="bg-cover object-cover h-auto w-full object-center"
-            />
-          </div>
+    <section aria-labelledby={name} className="flex flex-col relative">
+      <div>
+        <div className="max-h-52 max-w-[134px] md:max-w-[206px] m-auto md:max-h-72 object-center overflow-hidden">
+          <Image
+            alt={name}
+            src={images[0].url}
+            width={180}
+            height={251}
+            className="bg-cover object-cover h-auto w-full object-center"
+          />
         </div>
-        <div className="my-1 flex flex-col mx-10">
-          <h3 className="mt-1 text-xs" id={name}>
+      </div>
+      <div className="my-1 flex flex-col mx-10">
+        <h3 className="mt-1 text-xs" id={name}>
+          <Link
+            passHref
+            href={slug}
+            className="after:absolute after:w-full after:h-full after:content after:left-0 after:top-0 after:cursor-pointer"
+          >
             {name}
-          </h3>
-          <span className="font-medium text-sm md:text-md">
-            od {price / 100} zł
-          </span>
-          <div className="flex items-center justify-center w-full">
+          </Link>
+        </h3>
+        <span className="font-medium text-sm md:text-md">
+          od {price / 100} zł
+        </span>
+        <div className="flex gap-1 items-center justify-between relative">
+          <div>
             <Button className="md:text-xs rounded-none py-1 px-4 font-thin normal-case self-start">
               To Cart
             </Button>
+          </div>
 
-            <div className="relative block">
-              <FavoriteInput id={name} />
-            </div>
+          <div className="relative block">
+            <FavoriteInput id={name} />
           </div>
         </div>
-      </section>
-    </Link>
+      </div>
+    </section>
   );
 };
