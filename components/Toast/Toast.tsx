2589bd9ffdfdfe08c1802437883fd9b764ad36c6
Adam Cichowski <adam.cichowski98@outlook.com>
2023-02-05 13:08:20 +0100

    feat: add style to toast

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -3,14 +3,18 @@ import { ToastContainer } from "react-toastify";
 
 export function Toast() {
   return (
-    <div className="absolute left-0 top-0 w-full flex items-center">
-      <ToastContainer
-        limit={1}
-        bodyClassName="flex items-center h-6"
-        toastClassName="bg-primary text-white flex h-6"
-        className="w-1/2 left-0 top-0 m-auto h-6"
-        hideProgressBar
-      />
+    <div className="my-2 flex items-center">
+      <div className="absolute left-0 top-0 text-base w-full">
+        <ToastContainer
+          limit={1}
+          bodyClassName="flex gap-3 ml-2 items-center"
+          toastClassName="bg-primary font-semibold text-white flex rounded-md py-2 shadow shadow-primary"
+          className="left-0 top-0 mx-36"
+          closeButton
+          hideProgressBar
+          draggable={false}
+        />
+      </div>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,10 @@ import type { AppProps } from "next/app";
 import "public/fonts/fonts.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "/public/carrousel/carrousel.css";
-import "styles/toastify.css";
 import { CartContextProvider } from "context/CartContext/CartContext";
 import { Layout } from "components/Layout/Layout";
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "graphql/apolloClient";
-import { Toast } from "components/Toast/Toast";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient();
   return (
