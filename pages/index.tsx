1e2830632c788e49fa0f7996c22d7b7cc2a0de9b
Adam Cichowski <adam.cichowski98@outlook.com>
2023-03-12 08:37:50 +0100

    feat: divide rooms carrousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,46 +7,24 @@ import { Carrousel } from "components/Carrousel/Carrousel";
 import { SwiperSlide } from "swiper/react";
 import { settingsToEachCarrousel } from "components/Carrousel/constants";
 import { ProductOffer } from "components/ProductOffer/ProductOffer";
+import { RoomsCarrousel } from "views/home/components/RoomsCarrousel";
 const Home = () => {
   const { loading, error, data } =
     useQuery<GetProductsQuery>(GetProductsDocument);
-  if (error || !data?.products) {
-    return <p>{error?.message ?? "Problem to fetch products"}</p>;
-  }
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  // if (error || !data?.products) {
+  //   return <p>{error?.message ?? "Problem to fetch products"}</p>;
+  // }
 
   return (
     <div className="flex min-h-screen flex-col ">
-      <Carrousel
-        {...settingsToEachCarrousel.mainRoomsCarrousel}
-        renderSwiperSlide={() => (
-          <>
-            {["first", "second", "third"].map((el, i) => (
-              <SwiperSlide
-                aria-label="gallery"
-                className="min-w-screen aspect-video"
-                key={el}
-              >
-                <Image
-                  priority={!i}
-                  alt={`${el} room with posters`}
-                  layout="fill"
-                  src={`/carrousel/swiper-${i + 1}.jpg`}
-                />
-              </SwiperSlide>
-            ))}
-          </>
-        )}
-      />
+      <RoomsCarrousel isLoading={loading} />
       <section aria-labelledby="bestsellers">
         <h2 className="m-8 text-4xl font-medium" id="bestsellers">
           Bestsellers
         </h2>
         <ProductsCarrousel />
       </section>
-      {data.products.map((product) => (
+      {data?.products.map((product) => (
         <ProductOffer
           key={product.name}
           HeaderTag="h3"
---
+++ views/home/components/RoomsCarrousel.tsx
@@ -0,0 +1,48 @@
+import { Carrousel } from "components/Carrousel/Carrousel";
+import React from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { SpinIcon } from "components/Skeleton/SpinIcon";
+import { Autoplay, Navigation } from "swiper";
+
+const ROOMS_IMAGES: { src: string; alt: string }[] = [
+  { alt: "Beige room with mountain posters", src: "/carrousel/swiper-1.jpg" },
+  {
+    alt: "Posters with deers, inside in brown walls room",
+    src: "/carrousel/swiper-2.jpg",
+  },
+  {
+    alt: "Bright rooms with clearly and light posters",
+    src: "/carrousel/swiper-3.jpg",
+  },
+];
+
+export function RoomsCarrousel({ isLoading }: { isLoading: boolean }) {
+  if (isLoading) {
+    return (
+      <div className="min-w-screen flex aspect-video h-screen items-center justify-center">
+        <SpinIcon size="big" />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Swiper
+        speed={650}
+        navigation={true}
+        autoplay={{ delay: 3500, disableOnInteraction: true }}
+        modules={[Navigation, Autoplay]}
+      >
+        {ROOMS_IMAGES.map((image, i) => (
+          <SwiperSlide
+            aria-label="gallery"
+            className="min-w-screen aspect-video"
+            key={image.alt}
+          >
+            <Image priority={!i} fill {...image} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
