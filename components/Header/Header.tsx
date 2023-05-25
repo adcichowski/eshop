56760f785dd19f43c4e552d07cc8067d45e95998
Cichowski <adam.cichowski98@outlook.com>
2023-05-25 22:07:18 +0200

    feat: remove style from autocomplete

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
     <div className="h-14 sm:h-[100px] md:h-40">
       <header className="fixed z-50 w-full bg-white">
         <div className="min-w-md flex-column sticky z-40 grid w-full grid-cols-3 place-items-center items-center justify-around p-3 sm:p-4">
-          <Autocomplete className="ml-14" />
+          <Autocomplete />
           <div className="flex flex-col gap-3 text-center uppercase">
             <Link
               href="/"
