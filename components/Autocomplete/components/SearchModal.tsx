d3880cf7f27362ce1a3f0b514e37c5c8c5771269
Cichowski <adam.cichowski98@outlook.com>
2023-07-07 09:36:33 +0200

    feat: add poweredby search modal

--- components/Autocomplete/components/SearchModal.tsx
+++ components/Autocomplete/components/SearchModal.tsx
@@ -1,6 +1,6 @@
 import algoliasearch from "algoliasearch";
 import React, { useRef } from "react";
-import { Configure, InstantSearch } from "react-instantsearch-dom";
+import { Configure, InstantSearch, PoweredBy } from "react-instantsearch-dom";
 import { CustomHits } from "./CustomHits";
 import { CustomSearchBox } from "./SearchBox";
 import SearchIcon from "public/searchIcon.svg";
@@ -18,15 +18,15 @@ export default function SearchModal({
 }) {
   const ref = useRef<HTMLDivElement>(null);
 
-  useClickOutside(ref, handleModal);
+  // useClickOutside(ref, handleModal);
 
   return (
     <InstantSearch searchClient={searchClient()} indexName="eshop">
-      <div className="fixed top-0 z-40 h-full w-full animate-fade-down bg-white/75 duration-100">
-        <div className="mx-auto max-w-[1440px]">
+      <div className="fixed top-0 z-40 flex h-full w-full animate-fade-down flex-col bg-white/75 duration-100">
+        <div className="max-w-[1440px] self-stretch">
           <Configure clickAnalytics />
           <form
-            className="flex items-center justify-center gap-x-4 border-b-2 border-gray-100/25 bg-white py-6 px-2"
+            className="relative flex items-center justify-center gap-x-4 border-b-2 border-gray-100/25 bg-white py-7 px-2"
             aria-label="search"
           >
             <button type="button" className="h-6 w-6" onClick={handleModal}>
@@ -36,12 +36,13 @@ export default function SearchModal({
               ref={ref}
               className="flex max-w-md grow items-center self-center"
             >
-              <div role="search" className="max-w-md grow">
+              <div role="search" className="relative max-w-md grow">
                 <CustomSearchBox />
               </div>
               <button className="ml-2 h-6 w-6">
                 <SearchIcon />
               </button>
+              <PoweredBy className="absolute bottom-0 right-8 flex w-28 items-center gap-x-2 whitespace-nowrap text-xs" />
             </div>
           </form>
           <CustomHits />
