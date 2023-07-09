9651d63b51a2bf4b4d74c6c7843d8dffabbfbeec
Cichowski <adam.cichowski98@outlook.com>
2023-07-10 01:36:55 +0200

    feat: inert body if portal is open

--- components/Portal/usePortal.ts
+++ components/Portal/usePortal.ts
@@ -5,7 +5,20 @@ export function usePortal({ isOpen }: { isOpen: boolean }) {
   useEffect(() => {
     const modal = document.getElementById("__next");
     ref.current = modal;
-    document.body.style.overflow = isOpen ? "hidden" : "visible";
+    inertRootElement(isOpen);
+    setPaddingAfterLoseScroll();
   }, [isOpen]);
   return { ref };
 }
+
+const setPaddingAfterLoseScroll = () => {
+  const documentWidth = document.documentElement.clientWidth;
+  const windowWidth = window.innerWidth;
+  const scrollBarWidth = windowWidth - documentWidth;
+  document.body.style.paddingInlineEnd = `${Number(scrollBarWidth)} px`;
+};
+
+const inertRootElement = (isOpen: boolean) => {
+  const root = document.getElementById("root");
+  isOpen ? root?.setAttribute("inert", "true") : root?.removeAttribute("inert");
+};
