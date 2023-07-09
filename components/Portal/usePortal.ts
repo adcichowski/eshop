4b325f8612f13862fc794324a64ae24b33044a21
Cichowski <adam.cichowski98@outlook.com>
2023-07-10 01:46:17 +0200

    feat: include overflow to body

--- components/Portal/usePortal.ts
+++ components/Portal/usePortal.ts
@@ -15,10 +15,11 @@ const setPaddingAfterLoseScroll = () => {
   const documentWidth = document.documentElement.clientWidth;
   const windowWidth = window.innerWidth;
   const scrollBarWidth = windowWidth - documentWidth;
-  document.body.style.paddingInlineEnd = `${Number(scrollBarWidth)} px`;
+  document.body.style.paddingRight = `${Number(scrollBarWidth)} px`;
 };
 
 const inertRootElement = (isOpen: boolean) => {
   const root = document.getElementById("root");
+  document.body.style.overflow = isOpen ? "hidden" : "visible";
   isOpen ? root?.setAttribute("inert", "true") : root?.removeAttribute("inert");
 };
