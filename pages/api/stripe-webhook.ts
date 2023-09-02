dc25c8912ad237008f93717fb45f0626e0ae8028
Cichowski <adam.cichowski98@outlook.com>
2023-09-02 19:29:36 +0200

    fix: comment webhook

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -6,35 +6,32 @@ import { Stripe } from "stripe";
 import { getEnv } from "utils/utils";
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    const stripeKey = getEnv(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-      "NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY"
-    );
-    const stripe = new Stripe(stripeKey, { apiVersion: "2022-11-15" });
-    const body = await buffer(req);
-    const sig = req.headers["stripe-signature"];
-
-    if (typeof sig !== "string") {
-      throw new Error("Signature is not a string!");
-    }
-
-    const event = stripe.webhooks.constructEvent(
-      body,
-      sig,
-      getEnv(process.env.STRIPE_WEBHOOK, "STRIPE_WEBHOOK")
-    );
-
-    // Handle the event
-    eventStripeWebhook(event);
-
-    // Return a 200 response to acknowledge receipt of the event
-    res.status(200).end();
-  } catch (err) {
-    if (err instanceof Error) {
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-  }
+  // try {
+  //   const stripeKey = getEnv(
+  //     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  //     "NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY"
+  //   );
+  //   const stripe = new Stripe(stripeKey, { apiVersion: "2022-11-15" });
+  //   const body = await buffer(req);
+  //   const sig = req.headers["stripe-signature"];
+  //   if (typeof sig !== "string") {
+  //     throw new Error("Signature is not a string!");
+  //   }
+  //   const event = stripe.webhooks.constructEvent(
+  //     body,
+  //     sig,
+  //     getEnv(process.env.STRIPE_WEBHOOK, "STRIPE_WEBHOOK")
+  //   );
+  //   // Handle the event
+  //   eventStripeWebhook(event);
+  //   // Return a 200 response to acknowledge receipt of the event
+  //   res.status(200).end();
+  // } catch (err) {
+  //   if (err instanceof Error) {
+  //     return res.status(400).send(`Webhook Error: ${err.message}`);
+  //   }
+  // }
+  res.status(200).end();
 };
 
 export const config: PageConfig = {
