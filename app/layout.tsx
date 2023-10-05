ad1a7922c8880a6b40b87bff1873f660656c7079
Cichowski <adam.cichowski98@outlook.com>
2023-10-05 10:09:15 +0200

    feat: add next seo to global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,34 @@ import { Montserrat } from "next/font/google";
 import "../public/globals.css";
 import "swiper/css/bundle";
 import { Header } from "components/Header/Header";
+import type { Metadata } from "next";
+import { baseUrl } from "../constants";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   display: "swap",
 });
+
+export const metadata: Metadata = {
+  title: "PosterWall",
+  description: "Welcome to PosterWall",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  metadataBase: new URL(baseUrl),
+  openGraph: {
+    images: [
+      {
+        url: "/_next/image?url=%2Fcarrousel%2Fswiper-1.jpg&w=3840&q=75",
+        width: 1200,
+        height: 630,
+        alt: "PosterWall Shop",
+      },
+    ],
+  },
+};
+
 export default function GlobalLayout({ children }: { children: JSX.Element }) {
   return (
     <html lang="en" className={`${montserrat.className}`}>
