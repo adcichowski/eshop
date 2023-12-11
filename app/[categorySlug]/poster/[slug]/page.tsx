558012d77e06cd230ae1caa6b4edb92e6bc7a9a0
Cichowski <adam.cichowski98@outlook.com>
2023-12-11 07:53:21 +0100

    feat: metadata to product page

--- app/[categorySlug]/poster/[slug]/page.tsx
+++ app/[categorySlug]/poster/[slug]/page.tsx
@@ -3,6 +3,35 @@ import { ProductPage } from "./components/product/ProductPage";
 import { notFound } from "next/navigation";
 import { getProductBySlug } from "lib";
 import { getFavoriteProducts } from "lib/actions/favorite";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data = await getProductBySlug(params.slug);
+  return {
+    title: `Poster ${data.name}`,
+    description: data.description,
+    robots: {
+      index: true,
+      follow: true,
+    },
+    openGraph: {
+      title: `Poster ${data.name}`,
+      description: data.description,
+      images: [
+        {
+          url: data.image,
+          width: 430,
+          height: 630,
+          alt: "PosterWall Shop",
+        },
+      ],
+    },
+  } satisfies Metadata;
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const data = await getProductBySlug(params.slug);
